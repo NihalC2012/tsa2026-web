838300954da99ff3f5aefbdd1de252adf87696d3
sweetescape2025 <kovurshreyaan@gmail.com>
2026-01-07 22:40:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Timeline from "./Timeline";
 import Maps from "./Maps";
@@ -8,14 +8,13 @@ import Search from "./Search";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/events" element={<Timeline />} />
-      <Route path="/neighborhoods" element={<Maps />} />
-      <Route path="/people" element={<People />} />
-      <Route path="/search" element={<Search />} />
-      {/* fallback */}
-      <Route path="*" element={<Home />} />
-    </Routes>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/events" component={Timeline} />
+      <Route path="/neighborhoods" component={Maps} />
+      <Route path="/people" component={People} />
+      <Route path="/search" component={Search} />
+      <Route component={Home} />
+    </Switch>
   );
 }
